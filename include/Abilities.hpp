4a7d15849c4a4a0d4cd9efa6d6ecd4d7444c8c89
Nelson Billing <nbilling@gmail.com>
2012-03-29 19:00:38 +1100

    Implement thus-far unused ability invocation type.

--- include/Abilities.hpp
+++ include/Abilities.hpp
@@ -5,6 +5,26 @@
 #include "Formulas.hpp"
 #include "Object.hpp"
 
+typedef struct {
+  list<Coord>* path;
+} AbilityWalk;
+
+typedef struct {
+  int direction;
+} AbilityStep;
+
+enum AbilityType {
+  Walk, Step
+};
+
+struct Ability {
+  enum AbilityType type;
+  union {
+    AbilityWalk walk_data;
+    AbilityStep step_data;
+  } ability_data;
+};
+
 void walk (int obj_id, Zone* zone, list<Coord>* path);
 
 void step (int obj_id, Zone* zone, int direction);      
